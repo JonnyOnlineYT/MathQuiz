287a79bfb1880c8b7aeb2bb8e6b9dbdd96f38e9f
Jan <wasweissichwas1234@gmail.com>
2023-12-31 01:39:59 +0100

    added more math

--- src/main/java/me/jDev/mathQuiz/Util.java
+++ src/main/java/me/jDev/mathQuiz/Util.java
@@ -15,17 +15,36 @@ public class Util {
         }
         int a = new Random().nextInt(10)+1;
         int b = new Random().nextInt(10)+1;
+        String operation = "";
+        int c = 0;
+        switch (new Random().nextInt(3)) {
+            case 0: {
+                c = a+b;
+                operation = "+";
+                break;
+            }
+            case 1: {
+                c = a-b;
+                operation = "-";
+                break;
+            }
+            case 2: {
+                c = a*b;
+                operation = "*";
+                break;
+            }
+        }
         if (new Random().nextBoolean()) {
-            return new InputPanel(a+"+"+b, String.valueOf(a+b));
+            return new InputPanel(a+operation+b, String.valueOf(c));
         }
         int wrong1;
         do {
             wrong1 = new Random().nextInt(20)+1;
-        } while (wrong1 == a+b);
+        } while (wrong1 == c);
         int wrong2;
         do {
             wrong2 = new Random().nextInt(20)+1;
-        } while (wrong2 == a+b);
-        return new MultiAnswerPanel(a+"+"+b, new AnswerButton[] {new AnswerButton(a+b, true), new AnswerButton(wrong2, false), new AnswerButton(wrong1, false)});
+        } while (wrong2 == c);
+        return new MultiAnswerPanel(a+operation+b, new AnswerButton[] {new AnswerButton(c, true), new AnswerButton(wrong2, false), new AnswerButton(wrong1, false)});
     }
 }
